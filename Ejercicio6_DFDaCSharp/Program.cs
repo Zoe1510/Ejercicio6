0d4420f611f7cc735090c936b81d61f237ab14c2
Zoe1510 <patriciabermudez465@gmail.com>
2018-06-07 12:04:55 -0400

    first commit

---
+++ Ejercicio6_DFDaCSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6_DFDaCSharp
+{
+    class Program
+    {
+        /*6. Ejercicios de matrices: Cuadro mágico. 
+         
+         * Imprimir cómo se llena el cuadro y la suma de las filas, columnas y diagonales. 
+         
+         NOTA: Se puede utilizar cualquier función necesaria de C#.
+        */
+        static void Main(string[] args)
+        {
+        }
+    }
+}
